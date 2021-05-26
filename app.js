a4e7ba6ae101697e8cf3875a08678cffb56918a1
Yash4601 <53232830+Yash4601@users.noreply.github.com>
2021-05-27 02:40:00 +0530

    File Upload & Download

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const path = require('path');
 
+
 const express = require('express');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
 const flash = require('connect-flash');
+const {
+    v4: uuidv4
+} = require('uuid');
+const multer = require('multer');
 
 const errorController = require('./controllers/errorController');
 const User = require('./models/user');
 
-const MONGODB_URI = 'mongodb+srv://YashAgrawal:28uZXb8zHaElxUQa@cluster0.gxlzm.mongodb.net/shop?'
+const MONGODB_URI =
+    'mongodb+srv://YashAgrawal:28uZXb8zHaElxUQa@cluster0.gxlzm.mongodb.net/shop?'
 
 const app = express();
 const store = new MongoDBStore({
@@ -18,19 +24,38 @@ const store = new MongoDBStore({
     collection: 'sessions'
 })
 const csrfProtection = csrf();
-app.use(flash());
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, uuidv4() + '-' + file.originalname);
+    }
+})
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+}
 
 app.set('view engine', 'ejs');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
-const { error } = require('console');
 
 app.use(express.urlencoded({
     extended: false
 }));
+app.use(multer({
+    storage: fileStorage,
+    fileFilter: fileFilter
+}).single('image'))
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(session({
     secret: 'my secret',
     resave: false,
@@ -38,6 +63,7 @@ app.use(session({
     store: store
 }));
 app.use(csrfProtection);
+app.use(flash());
 app.use((req, res, next) => {
     res.locals.isAuthenticated = req.session.isLoggedIn;
     res.locals.csrfToken = req.csrfToken();
@@ -53,7 +79,7 @@ app.use((req, res, next) => {
             if (!user) {
                 return next();
             }
-            req.user = user;   
+            req.user = user;
             next();
         })
         .catch(err => {
@@ -70,6 +96,7 @@ app.get('/500', errorController.get500Page);
 app.use(errorController.get404Page);
 
 app.use((error, req, res, next) => {
+    // res.redirect('/500');
     res.status(500).render('500', {
         pageTitle: 'Error!',
         path: '/500',
